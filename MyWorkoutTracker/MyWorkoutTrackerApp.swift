88286b580ae94deefc1166597b7644c74147d8ee
SamuelNaumann <samuel134.naumann@gmail.com>
2025-08-19 20:49:01 +0200

    Initial Commit

---
+++ MyWorkoutTracker/MyWorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutTrackerApp.swift
+//  MyWorkoutTracker
+//
+//  Created by Samuel Naumann on 2025-08-19.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyWorkoutTrackerTests/MyWorkoutTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyWorkoutTrackerTests.swift
+//  MyWorkoutTrackerTests
+//
+//  Created by Samuel Naumann on 2025-08-19.
+//
+
+import Testing
+
+struct MyWorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
