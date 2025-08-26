edaa89ddb5fddbc29b62c5a42a9f8af75f133d11
SamuelNaumann <samuel134.naumann@gmail.com>
2025-08-26 11:47:49 +0200

    Add SwitData workout model

--- MyWorkoutTracker/MyWorkoutTrackerApp.swift
+++ MyWorkoutTracker/MyWorkoutTrackerApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct MyWorkoutTrackerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+        }.modelContainer(for: Workout.self)
     }
 }
